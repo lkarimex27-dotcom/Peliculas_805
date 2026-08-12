eca34c35655ee5941fbec2f24d1086ec1d8888c6
sofiagonzalesortiz13 <sofiagonzalezort13@gmail.com>
2026-08-12 07:15:13 -0500

    menu

---
+++ Menu.dart
@@ -0,0 +1,45 @@
+import 'dart:io' show stdin, stdout ;
+import 'Eliminar.dart' as eliminar;
+import 'Listar.dart' as listar;
+
+void main() {
+  List<Map<String, dynamic>> pelicula = [];
+  int? opcion;
+
+  do {
+    print("\nPELICULAS");
+    print("1. Agregar pelicula");
+    print("2. Listar pelicula");
+    print("3. Actualizar pelicula");
+    print("4. Eliminar pelicula");
+    print("5. Salir");
+
+    stdout.write("Seleccione una opción: ");
+    opcion = int.tryParse(stdin.readLineSync() ?? "");
+
+    switch (opcion) {
+      case 1:
+        agregarPelicula(pelicula);
+        break;
+
+      case 2:
+        listar.peliculas(pelicula);
+        break;
+
+      case 3:
+        actualizarPelicula(pelicula);
+        break;
+
+      case 4:
+        eliminar.peliculas(pelicula);
+        break;
+
+      case 5:
+        print("Programa finalizado.");
+        break;
+
+      default:
+        print("Opción inválida. Intente nuevamente.");
+    }
+  } while (opcion != 5);
+  }
